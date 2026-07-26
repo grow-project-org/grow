51297f7b027898b8f1c15d00e835a88712c82092
Michał <michalfr1601@gmail.com>
2026-07-26 19:58:40 +0200

    added tests integration

---
+++ backend/Grow.Tests.Integration/PlantsControllerTest.cs
@@ -0,0 +1,53 @@
+﻿using Grow.WebApi.Endpoints;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace Grow.Tests.Integration;
+
+[TestFixture]
+public class PlantsControllerTest
+{
+    private HttpClient _client;
+    private WebApplicationFactory<Program> _factory;
+
+    [SetUp]
+    public void SetUp()
+    {
+        this._factory = new WebApplicationFactory<Program>()
+            .WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                });
+            });
+
+        this._client = this._factory.CreateClient();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        this._client.Dispose();
+        this._factory.Dispose();
+    }
+
+    [Test]
+    public async Task Post_Plants_ShouldReturnPlantId()
+    {
+        var newPlantCommand = new
+        {
+            CustomId = "monstera-01",
+            SpecieId = Guid.NewGuid()
+        };
+
+        var response = await this._client.PostAsJsonAsync("/api/plants", newPlantCommand);
+
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+        var result = await response.Content.ReadFromJsonAsync<CreatePlantResponse>();
+
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result!.CreatedPlantId, Is.Not.EqualTo(Guid.Empty));
+    }
+}
