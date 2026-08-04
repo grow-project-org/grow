9405a9bc054b97ce8eb302db162e4abc6ac9fb74
Marcin99b <marcin.bigos99@gmail.com>
2026-08-04 23:17:46 +0200

    create specie

--- backend/Grow.Domain/IDatabaseContext.cs
+++ backend/Grow.Domain/IDatabaseContext.cs
@@ -1,10 +1,16 @@
 ﻿using Grow.Domain.Plants;
+using Grow.Domain.Species;
+using Grow.Domain.Users;
 using Microsoft.EntityFrameworkCore;
 
 namespace Grow.Domain;
 
 public interface IDatabaseContext
 {
-    DbSet<Plant> Plants { get; }
+    public DbSet<Plant> Plants { get; set; }
+    public DbSet<Specie> Species { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<PlantGroup> PlantGroups { get; set; }
+    public DbSet<PlantEvent> PlantEvents { get; set; }
     Task<int> SaveChangesAsync(CancellationToken ct = default);
 }
---
+++ backend/Grow.WebApi/Endpoints/SpeciesEndpoints.cs
@@ -0,0 +1,27 @@
+﻿using Grow.Domain.Species.Handlers;
+using Grow.Infrastructure.Cqrs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Grow.WebApi.Endpoints;
+
+public record CreateSpecieRequest(string Name);
+public record CreateSpecieResponse(Guid SpecieId);
+
+public static class SpeciesEndpoints
+{
+    public static IEndpointRouteBuilder MapSpeciesEndpoints(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("/api/species").WithTags("Species");
+
+        _ = group.MapPost("/", CreateSpecie);
+
+        return app;
+    }
+
+    public static async Task<CreateSpecieResponse> CreateSpecie(IDispatcher dispatcher, [FromBody] CreateSpecieRequest request, CancellationToken ct)
+    {
+        var id = Guid.CreateVersion7();
+        await dispatcher.SendAsync(new CreateSpecieCommand(id, request.Name), ct);
+        return new(id);
+    }
+}
