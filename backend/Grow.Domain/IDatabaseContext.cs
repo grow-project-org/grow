214259ec37f8b0deac11544d03c2101be98c1197
Marcin99b <marcin.bigos99@gmail.com>
2026-08-04 23:20:19 +0200

    interface internal

--- backend/Grow.Domain/IDatabaseContext.cs
+++ backend/Grow.Domain/IDatabaseContext.cs
@@ -7,10 +7,10 @@ namespace Grow.Domain;
 
 public interface IDatabaseContext
 {
-    public DbSet<Plant> Plants { get; set; }
-    public DbSet<Specie> Species { get; set; }
-    public DbSet<User> Users { get; set; }
-    public DbSet<PlantGroup> PlantGroups { get; set; }
-    public DbSet<PlantEvent> PlantEvents { get; set; }
+    DbSet<Plant> Plants { get; set; }
+    DbSet<Specie> Species { get; set; }
+    DbSet<User> Users { get; set; }
+    DbSet<PlantGroup> PlantGroups { get; set; }
+    DbSet<PlantEvent> PlantEvents { get; set; }
     Task<int> SaveChangesAsync(CancellationToken ct = default);
 }
