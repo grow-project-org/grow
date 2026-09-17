2d5bb132b590cc3e27ac1a1e2c4da9a440946633
Marcin99b <marcin.bigos99@gmail.com>
2026-09-17 16:24:49 +0200

    users endpoints

--- backend/Grow.WebApi/Endpoints/UsersEndpoints.cs
+++ backend/Grow.WebApi/Endpoints/UsersEndpoints.cs
@@ -1,11 +1,23 @@
-﻿using Grow.Domain.Users.Handlers;
+﻿using Grow.Commons.Throttling;
+using Grow.Domain.Users.Handlers;
+using Grow.Infrastructure.Auth;
 using Grow.Infrastructure.Cqrs;
+using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+using System.Security.Claims;
+using static System.Net.WebRequestMethods;
 
 namespace Grow.WebApi.Endpoints;
 
 public record CreateUserRequest(string Email, string Username);
 public record CreateUserResponse(Guid Id);
+
+public record MeResponse(string Username);
 
 public static class UsersEndpoints
 {
@@ -17,13 +29,65 @@ public static class UsersEndpoints
         _ = group.MapPost("/login", Register);
         _ = group.MapPost("/me", Register);
 
+        group.MapGet("/csrf", (IAntiforgery forgery, HttpContext ctx) =>
+        {
+            var tokens = forgery.GetAndStoreTokens(ctx);
+            return tokens.RequestToken;
+        }).AllowAnonymous();
+
         return app;
     }
 
-    public static async Task<CreateUserResponse> Register(IDispatcher dispatcher, [FromBody] CreateUserRequest request, CancellationToken ct)
+    public static async Task Register(IDispatcher dispatcher, UniversalThrottle throttle, HttpContext http, [FromBody] CreateUserRequest request, CancellationToken ct)
     {
+        if (!throttle.TryAcquire(nameof(Register), request.Email))
+        {
+            http.Response.StatusCode = 429;
+            return;
+        }
+
         var id = Guid.CreateVersion7();
         await dispatcher.SendAsync(new CreateUserCommand(id, request.Email, request.Username), ct);
-        return new(id);
+    }
+
+    public static async Task Login(IDispatcher dispatcher, UniversalThrottle throttle, HttpContext http, SessionStorage sessionStorage, [FromBody] LoginRequest request, CancellationToken ct)
+    {
+        if (!throttle.TryAcquire(nameof(Login), request.Email))
+        {
+            http.Response.StatusCode = 429;
+            return;
+        }
+
+        var user = (await dispatcher.QueryAsync<GetUserByEmailQuery, GetUserByEmailQueryResult>(new GetUserByEmailQuery(request.Email), ct)).User;
+
+        //todo add real confirmation
+        //this is shortcut for MVP
+        var confirmationKey = sessionStorage.CreateSession(new AuthUser(user!.Id, true), http);
+        var sessionKey = sessionStorage.ConfirmSession(confirmationKey, http);
+
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.NameIdentifier, sessionKey),
+        };
+
+        var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+        var principal = new ClaimsPrincipal(identity);
+
+        await http.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+    }
+
+    public static Task Logout(HttpContext http, UserSessionProvider sessionProvider, CancellationToken ct)
+    {
+        ct.ThrowIfCancellationRequested();
+        sessionProvider.Logout();
+        return http.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    }
+
+    public static async Task<MeResponse> GetMe(IDispatcher dispatcher, HttpContext http, UserSessionProvider sessionProvider, CancellationToken ct)
+    {
+        var session = sessionProvider.GetSession(http);
+        var user = (await dispatcher.QueryAsync<GetUserByIdQuery, GetUserByIdQueryResult>(new GetUserByIdQuery(session.UserId), ct)).User;
+
+        return new MeResponse(user!.Username);
     }
 }
