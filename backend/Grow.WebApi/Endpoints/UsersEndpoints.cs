f50dfe71c15338f8f9de904ad0d45d495961d923
Marcin99b <marcin.bigos99@gmail.com>
2026-09-17 16:06:17 +0200

    add register endpoint

---
+++ backend/Grow.WebApi/Endpoints/UsersEndpoints.cs
@@ -0,0 +1,27 @@
+﻿using Grow.Domain.Users.Handlers;
+using Grow.Infrastructure.Cqrs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Grow.WebApi.Endpoints;
+
+public record CreateUserRequest(string Email, string Username);
+public record CreateUserResponse(Guid Id);
+
+public static class UsersEndpoints
+{
+    public static IEndpointRouteBuilder MapUsersEndpoints(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("/api/users").WithTags("Users");
+
+        _ = group.MapPost("/register", Register);
+
+        return app;
+    }
+
+    public static async Task<CreateUserResponse> Register(IDispatcher dispatcher, [FromBody] CreateUserRequest request, CancellationToken ct)
+    {
+        var id = Guid.CreateVersion7();
+        await dispatcher.SendAsync(new CreateUserCommand(id, request.Email, request.Username), ct);
+        return new(id);
+    }
+}
