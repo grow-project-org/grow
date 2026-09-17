af44824f9b30bc9538542526f6ac9797a0e92221
Michał <michalfr1601@gmail.com>
2026-09-17 18:13:20 +0200

    add plant action schedule calculation

---
+++ backend/Grow.Domain/Plants/Handlers/GetPlantActionScheduleQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using Grow.Domain.Commons;
+using Grow.Infrastructure.Cqrs;
+using Microsoft.EntityFrameworkCore;
+
+namespace Grow.Domain.Plants.Handlers;
+
+public record GetPlantActionScheduleQuery(Guid PlantId, PlantActionType Type) : IQuery<GetPlantActionScheduleQueryResult>;
+public record GetPlantActionScheduleQueryResult(DateOnly? NextDate);
+
+public class GetPlantActionScheduleQueryHandler(IDatabaseContext databaseContext) : IQueryHandler<GetPlantActionScheduleQuery, GetPlantActionScheduleQueryResult>
+{
+    public async Task<GetPlantActionScheduleQueryResult> HandleAsync(GetPlantActionScheduleQuery query, CancellationToken ct)
+    {
+        var plant = await databaseContext.Plants.FirstAsync(x => x.Id == query.PlantId, ct);
+        var specie = await databaseContext.Species.FirstAsync(x => x.Id == plant.SpecieId, ct);
+
+        var lastEvent = await databaseContext.PlantEvents
+            .Where(x => x.PlantId == plant.Id && x.Type == query.Type)
+            .OrderByDescending(x => x.ExecutedAt)
+            .FirstOrDefaultAsync(ct);
+
+        if (lastEvent is null)
+        {
+            return new GetPlantActionScheduleQueryResult(null);
+        }
+
+        if (!specie.Intervals.TryGetValue(query.Type, out var interval))
+        {
+            return new GetPlantActionScheduleQueryResult(null);
+        }
+
+        var nextDate = PlantActionSchedule.CalculateNextDate(lastEvent.ExecutedAt, interval);
+
+        return new GetPlantActionScheduleQueryResult(nextDate);
+    }
+}
---
+++ backend/Grow.Domain/Plants/PlantActionSchedule.cs
@@ -0,0 +1,10 @@
+﻿namespace Grow.Domain.Plants;
+
+public class PlantActionSchedule
+{
+    public static DateOnly CalculateNextDate(DateTime executedAt, TimeSpan interval)
+    {
+        var nextDate = executedAt + interval;
+        return DateOnly.FromDateTime(nextDate);
+    }
+}
