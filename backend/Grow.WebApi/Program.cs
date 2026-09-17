c2216d2130e5d82bfbd1fdbc75dfb1b9ee88e8f5
Marcin99b <marcin.bigos99@gmail.com>
2026-09-17 16:33:36 +0200

    fix hc

--- backend/Grow.WebApi/Program.cs
+++ backend/Grow.WebApi/Program.cs
@@ -69,37 +69,6 @@ if (app.Environment.IsDevelopment())
     _ = app.MapOpenApi();
 }
 
-//todo setup cors to hide healthcheck
-//it should be available for local network only
-app.MapHealthChecks("/hc", new HealthCheckOptions()
-{
-    ResponseWriter = async (context, report) =>
-    {
-        context.Response.ContentType = "application/json; charset=utf-8";
-
-        var response = new
-        {
-            status = report.Status.ToString(),
-            totalDuration = report.TotalDuration.TotalMilliseconds,
-            checks = report.Entries.Select(e => new
-            {
-                name = e.Key,
-                status = e.Value.Status.ToString(),
-                duration = e.Value.Duration.TotalMilliseconds,
-                description = e.Value.Description,
-                error = e.Value.Exception?.Message,
-            })
-        };
-
-        await context.Response.WriteAsync(
-            JsonSerializer.Serialize(response, new JsonSerializerOptions
-            {
-                WriteIndented = true,
-                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
-            }));
-    }
-});
-
 app.UseCors();
 app.UseRateLimiter();
 app.AddHealthChecks();
